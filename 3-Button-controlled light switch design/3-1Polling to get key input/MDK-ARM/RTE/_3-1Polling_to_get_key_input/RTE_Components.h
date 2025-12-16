4cccf664d63c275de6a57103e1695bbebe6f23a9
Eric-B550MK <jff_16@163.com>
2025-12-16 19:37:52 +0800

    任务3.1轮询式获取按键输入测试完毕

---
+++ 3-Button-controlled light switch design/3-1Polling to get key input/MDK-ARM/RTE/_3-1Polling_to_get_key_input/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '3-1Polling to get key input' 
+ * Target:  '3-1Polling to get key input' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
