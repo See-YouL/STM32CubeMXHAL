48d0177c762b0bcc1b217594a2c71427321091b1
Eric-B550MK <jff_16@163.com>
2026-04-16 20:33:15 +0800

    8.1 定时器部分代码

---
+++ 8/8-1/Core/Inc/Timer.h
@@ -0,0 +1,15 @@
+#ifndef __TIMER_H
+#define __TIMER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "main.h"
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __TIMER_H */
---
+++ 8/8-1/Core/Src/Timer.c
@@ -0,0 +1,13 @@
+#include "Timer.h"
+
+extern TIM_HandleTypeDef htim2;
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef * htim)
+{
+  if (htim == &(htim2)) // 判断是否是定时器2 的中断回调
+  {
+    LED_RED = !LED_RED; // 红灯闪烁
+    Uart1ReceiveFlag = 1; // 设置 UART 接收完成标志
+    HAL_TIM_Base_Stop_IT(&htim2); // 停止定时器中断
+  }
+}
